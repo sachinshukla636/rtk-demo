2a34d2802a763e916b1dacfb08a6b06e1c8392d3
sachinshukla636 <sachinshukla636@gmail.com>
2023-12-16 19:13:57 +0530

    Logger Middleware

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const icecreamActions =
 
 console.log("Initial state", store.getState());
 const unsubscribe = store.subscribe(() => {
-  console.log("Updated state", store.getState());
+//   console.log("Updated state", store.getState());
 });
 
 store.dispatch(cakeActions.ordered());
